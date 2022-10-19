93a04cf151e4313679dc4728b2d6633f3a26fa38
Javier Padin <javier.padin@solera.com>
2022-10-19 10:54:07 +0200

    New Test: modify description

--- ToDoTests/UnitTest1.cs
+++ ToDoTests/UnitTest1.cs
@@ -33,5 +33,30 @@ namespace ToDoTests
             //$"{result.ID} does not exist"
             Assert.That(result, Is.Null);
         }
+
+        [Test]
+        public void ModifyDescription_NewDescription_SuccesfullyModifiesTheNote()
+        {
+            string newDescr = "Money spent";
+            var note = new Note()
+            {
+                ID = "123",
+                Description = "Money expenses",
+                Text = "To be paid to Kentucky Chicken Wings: 2000€",
+                Created = "2020-01-01",
+                EstimatedCompletion = "2023-01-01",
+                DateOfCompletion = "",
+                Link = "",
+                Mentions = null,
+                Priority = NotePriorities.High
+            };
+
+            var controller = new ToDoController();
+            controller.ModifDescription("123", newDescr);
+
+            Note result = controller.ReadNote("123");
+
+            Assert.That(result.Description, Is.EqualTo(newDescr));
+        }
     }
 }
